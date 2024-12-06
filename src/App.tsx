3ab8fbd9b2907c7451cbc3f00874f63c02727593
arash_jfrz <arashjfrz75@gmail.com>
2024-12-06 15:39:16 +0330

    third

--- src/App.tsx
+++ src/App.tsx
@@ -3,41 +3,6 @@ import { useEffect } from 'react'
 import './App.css'
 
 
-
-// export function App() {
-//   const [count, setCount] = useState(1)
-//   useEffect(() => {
-//     setTimeout(() => {
-//       setCount(2);
-//     }, 5000);
-//   }, []);
-//   console.log(count)
-
-//   function nextPage() {
-//     setCount(count + 1)
-//   }
-//   return (
-//     <>
-//       {count == 1 && <>
-//         <div className="felx justify-center items-center">
-//           <img src="public/logo.png" alt="" className="flex bg-white w-[60px] h-[60px] rounded-full p-3" />
-//           <h1 className='flex mt-50px mr-60px text-3xl'>Shoea</h1>
-//         </div>
-//         <div className='flex justify-center items-center mt-[80%]'>
-//           <img src="public/spinner-atom.png" alt="" className=' animate-spin transition-[2s]' />
-//         </div>
-//       </>
-//       }
-//       {count == 2 &&
-//         <img src="public/onboarding page 2.png" alt="welcome" />
-//       }
-//     </>
-
-//   )
-
-// }
-
-
 export function App() {
   const [count, setCount] = useState(1);
 
